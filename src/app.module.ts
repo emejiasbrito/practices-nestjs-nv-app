cb311eaf67266201d83d096b595d2bef46995c9f
emejiasbrito <88601791+emejiasbrito@users.noreply.github.com>
2023-03-30 00:08:19 -0300

    feat(config) files config init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from 'app.controller';
+import { AppService } from 'app.service';
+import config from '../config';
+import { environments } from '../environment';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
